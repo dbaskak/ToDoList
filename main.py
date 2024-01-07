a4db7b6fa96a9e824508cc29689285c31d9522b0
dbaskak <dbaskak@gmail.com>
2024-01-07 08:16:47 +0200

    class TodoList filled

--- main.py
+++ main.py
@@ -10,27 +10,39 @@ class TodoList:
 
     def load_tasks(self):
         # Load tasks from the file or create an empty list if the file doesn't exist or is empty
-
+        try:
+            with open(self.filename, 'r') as file:
+                tasks = json.load(file)
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            tasks = []
+        return tasks
 
     def add_task(self, task, deadline=None):
         # Add a new task to the list
-
+        self.tasks.append({'task': task, 'done': False, 'deadline': deadline, 'subtask': []})
+        # Save the updated tasks
+        self.save_tasks()
 
     def save_tasks(self):
         # Save tasks to the file
-
+        with open(self.filename, 'w') as file:
+            json.dump(self.tasks, file)
 
     def remove_task(self, index):
         # Remove a task at the specified index
-
+        if 0 <= index < len(self.tasks):
+            del self.tasks[index]
+            # Save the updated tasks
+            self.save_tasks()
 
     def mark_as_down(self, index):
         # Mark a task as done at the specified index
 
-
     def add_subtask(self, index, subtask):
-        # Add a subtask to the task at the specified index
-
+        # Mark a task as done at the specified index
+        if 0 <= index <= len(self.tasks):
+            self.tasks[index]['done'] = True
+            # Save the updated tasks
 
 class TodoApp:
     def __init__(self, root):
